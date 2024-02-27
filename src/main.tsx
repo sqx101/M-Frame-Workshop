324d26deee030fff8686b8b9efd1298e4e19db4a
sqx101 <145257477+sqx101@users.noreply.github.com>
2024-02-27 06:39:44 -0600

    end of session 3 code

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,52 @@
 /*
- * Add button renderer
- *
- * - ButtonRenderer function maps over an array of strings to display
- *   a given frame's buttons (max of 4)
- *
- * - render metatags with the fc button option using
- *   the index + 1 (since js arrays are zero based)
- */
+* Adding interactivity
+*
+* - Add post route handler as defined in the layout
+* - Warpcast will send a object in this format:
+* {
+    "untrustedData": {
+      "fid": 2,
+      "url": "https://fcpolls.com/polls/1",
+      "messageHash": "0xd2b1ddc6c88e865a33cb1a565e0058d757042974",
+      "timestamp": 1706243218,
+      "network": 1,
+      "buttonIndex": 2,
+      "inputText": "hello world", // "" if requested and no input, undefined if input not requested
+      "castId": {
+        "fid": 226,
+        "hash": "0xa48dd46161d8e57725f5e26e34ec19c13ff7f3b9"
+      }
+    },
+    "trustedData": {
+      "messageBytes": "d2b1ddc6c88e865a33cb1a565e0058d757042974..."
+    }
+  }
+
+*
+* We compose our dynamic logic around which buttonIndex is sent back
+* and render a different frame depending on which button index is send
+*
+* Since the data is being send over the wire, we are checking that it does
+* in fact exist and if not, throwing an error
+*
+*/
 
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+
+type FrameDataRes = {
+  fid: number;
+  url: string;
+  messageHash: string;
+  timestamp: number;
+  network: number;
+  buttonIndex: 1 | 2 | 3 | 4;
+  castId: {
+    fid: number;
+    hash: string;
+  };
+};
 
 const app = new Hono();
 
@@ -21,8 +58,16 @@ const ButtonRenderer = ({ buttonData }: { buttonData: string[] }) => (
   </>
 );
 
-const Layout = ({ imgUrl, postUrl }: { imgUrl: string; postUrl: string }) => {
-  const buttonData = ["Btn 1", "Btn 2", "Btn 3", "Btn 4"];
+const Layout = ({
+  imgUrl,
+  postUrl,
+  buttonData,
+}: {
+  imgUrl: string;
+  postUrl?: string;
+  buttonData?: string[];
+}) => {
+  const defaultButtonData = ["Btn 1", "Btn 2", "Btn 3", "Btn 4"];
   return (
     <html>
       <head>
@@ -30,8 +75,11 @@ const Layout = ({ imgUrl, postUrl }: { imgUrl: string; postUrl: string }) => {
         <meta property="og:image" content={`${imgUrl}`} />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content={`${imgUrl}`} />
-        <meta property="fc:frame:post_url" content={postUrl} />
-        {<ButtonRenderer buttonData={buttonData} />}
+        <meta
+          property="fc:frame:post_url"
+          content={postUrl || "https://frame-workshop.up.railway.app/res"}
+        />
+        {<ButtonRenderer buttonData={buttonData || defaultButtonData} />}
       </head>
     </html>
   );
@@ -45,6 +93,38 @@ app.get("/", (c) =>
     />,
   ),
 );
+
+app.post("/res", async (c) => {
+  const frameData: { untrustedData: FrameDataRes } = await c.req.json();
+
+  console.log(frameData);
+
+  if (!frameData?.untrustedData?.buttonIndex) {
+    throw new HTTPException(400, { message: "frame data missing" });
+  }
+
+  const { buttonIndex } = frameData.untrustedData;
+
+  switch (buttonIndex) {
+    case 1: {
+      return c.render(<Layout imgUrl="https://i.imgur.com/g4Ll2Dj.png" />);
+    }
+    case 2: {
+      return c.render(<Layout imgUrl="https://i.imgur.com/mnSybOx.jpeg" />);
+    }
+    case 3: {
+      return c.render(<Layout imgUrl="https://i.imgur.com/eOw6ff3.jpeg" />);
+    }
+    case 4: {
+      return c.render(<Layout imgUrl="https://i.imgur.com/5CBUsN1.jpeg" />);
+    }
+
+    default: {
+      //const buttonData = ["Btn 1 d", "Btn 2 d", "Btn 3 d", "Btn 4 d"]
+      return c.render(<Layout imgUrl="https://i.imgur.com/sS717ci.jpg" />);
+    }
+  }
+});
 
 console.log("sever is running");
 serve({
